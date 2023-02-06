bfa7b199dfe7f42eab8796284a2f92ec223d5cd6
Michael Pan <panmpan@gmail.com>
2023-02-06 08:59:33 +0800

    準備輸出
    加入教學

--- Assets/Scripts/UI/MenuControl.cs
+++ Assets/Scripts/UI/MenuControl.cs
@@ -7,6 +7,8 @@ using XnodeBehaviourTree;
 
 public class MenuControl : MonoBehaviour
 {
+    public static bool s_showMoreInfoButton = false;
+
     [Header("Others")]
     [SerializeField]
     private BossBehaviour bossBehaviour;
@@ -15,18 +17,35 @@ public class MenuControl : MonoBehaviour
     [SerializeField]
     private PlayerInput playerInput;
 
-    [Header("Menu")]
+    [Header("Start")]
     [SerializeField]
     private GameObject startMenu;
     [SerializeField]
     private GameObject startButton;
     [SerializeField]
     private GameObject creditButton;
+
+    [Header("Credit")]
     [SerializeField]
     private GameObject creditMenu;
     [SerializeField]
     private GameObject creditClose;
 
+    [Header("Tutorial")]
+    [SerializeField]
+    private GameObject tutorial;
+    [SerializeField]
+    private GameObject tutorialStartButton;
+    [SerializeField]
+    private GameObject showMoreButton;
+
+    [Header("More Info")]
+    [SerializeField]
+    private GameObject moreInfo;
+    [SerializeField]
+    private GameObject moreInfoStartButton;
+
+    [Header("Ending")]
     [SerializeField]
     private GameObject win;
     [SerializeField]
@@ -49,6 +68,11 @@ public class MenuControl : MonoBehaviour
         playerInput.enabled = false;
 
         startMenu.SetActive(true);
+        creditMenu.SetActive(false);
+        tutorial.SetActive(false);
+        moreInfo.SetActive(false);
+        showMoreButton.SetActive(s_showMoreInfoButton);
+
         win.SetActive(false);
         lose.SetActive(false);
 
@@ -57,7 +81,15 @@ public class MenuControl : MonoBehaviour
 
     public void StartTutorial()
     {
-        StartGame();
+        tutorial.SetActive(true);
+        EventSystem.current.SetSelectedGameObject(tutorialStartButton);
+    }
+
+    public void StartMoreInfo()
+    {
+        tutorial.SetActive(false);
+        moreInfo.SetActive(true);
+        EventSystem.current.SetSelectedGameObject(moreInfoStartButton);
     }
 
     public void StartGame()
@@ -66,6 +98,8 @@ public class MenuControl : MonoBehaviour
         bossBehaviour.enabled = true;
         bossBehaviourTreeRunner.enabled = true;
         playerInput.enabled = true;
+
+        s_showMoreInfoButton = true;
     }
 
     public void OpenCredit()
